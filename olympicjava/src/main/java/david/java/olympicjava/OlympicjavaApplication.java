a70dbd778d7e13368c970817850cab71f9da8cf3
David LI <108392457+BanggEddy@users.noreply.github.com>
2024-04-18 10:16:49 +0200

    init project

---
+++ olympicjava/src/main/java/david/java/olympicjava/OlympicjavaApplication.java
@@ -0,0 +1,13 @@
+package david.java.olympicjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlympicjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlympicjavaApplication.class, args);
+	}
+
+}
---
+++ olympicjava/src/test/java/david/java/olympicjava/OlympicjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package david.java.olympicjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlympicjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
